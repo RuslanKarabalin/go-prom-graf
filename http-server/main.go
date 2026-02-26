914f4e9c8b991428cfa9acdda4270c3af63a397f
Ruslan Karabalin <karabalin112@gmail.com>
2026-02-26 09:35:46 +0300

    feat: add rwmutex and better responses

--- http-server/main.go
+++ http-server/main.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
+	"sync"
 )
 
 type someType struct {
@@ -14,12 +15,15 @@ type someType struct {
 type server struct {
 	nextId int
 	somes  map[int]someType
+	mtx    sync.RWMutex
 }
 
 func getSomeHandler(s *server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		slog.Info("Call 'getSomeHandler'")
 		w.Header().Set("Content-Type", "application/json")
+		s.mtx.RLock()
+		defer s.mtx.RUnlock()
 		if err := json.NewEncoder(w).Encode(s.somes); err != nil {
 			slog.Error("Can't encode somes", slog.Any("error", err))
 		}
@@ -40,9 +44,15 @@ func postSomeHandler(s *server) http.HandlerFunc {
 			http.Error(w, "Can't decode body", http.StatusBadRequest)
 			return
 		}
+
+		s.mtx.Lock()
+		defer s.mtx.Unlock()
 		s.somes[s.nextId] = tmp
 		s.nextId++
+
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusCreated)
+		_ = json.NewEncoder(w).Encode(map[string]int{"id": s.nextId - 1})
 	}
 }
 
@@ -69,12 +79,15 @@ func putSomeHandler(s *server) http.HandlerFunc {
 			return
 		}
 
+		s.mtx.Lock()
+		defer s.mtx.Unlock()
 		if _, exists := s.somes[id]; !exists {
 			slog.Error("Can't find id")
 			http.Error(w, "Can't find id", http.StatusNotFound)
 			return
 		}
 		s.somes[id] = tmp
+		w.WriteHeader(http.StatusNoContent)
 	}
 }
 
